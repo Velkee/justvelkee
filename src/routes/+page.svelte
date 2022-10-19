7e630199a67a2055b71c92681d809acbeebdcb2d
Fenic <89312879+Velkee@users.noreply.github.com>
2022-10-19 13:27:11 +0200

    Moved to Svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<header>
+	<h1>Fenic's Corner</h1>
+</header>
+
+<div class="centered_justify">
+	<h2>// About Me //</h2>
+	<h3>Hi! I'm Fenic, also know as Velkee</h3>
+	<p>I'm an ITK student with a love for open source technology, video games, and digital art.</p>
+</div>
+
+<div class="centered_justify">
+	<h2>// My Projects //</h2>
+	<p>
+		These are the projects I've worked on in my free time, either just for the love of it or because
+		it served a specific purpose when I made it.
+	</p>
+
+	<div class="waffles">
+		<a href="/tasv">
+			<h3>T.A.S.V.</h3>
+		</a>
+	</div>
+</div>
+
+<div class="centered_justify">
+	<h2>// Contributions //</h2>
+	<p>
+		These are some of the repositories I have contributed towards. A lot of my friend's repositories
+		will probably be here.
+	</p>
+
+	<div class="waffles">
+		<a href="https://www.github.com/CheesyPhoenix/BetterSchool">
+			<h3>BetterSchool</h3>
+		</a>
+	</div>
+</div>
---
+++ src/routes/tasv/+page.svelte
@@ -0,0 +1,28 @@
+<header>
+	<h1>T.A.S.V.</h1>
+
+	<nav>
+		<a href="/">
+			<p>Back to Home</p>
+		</a>
+	</nav>
+</header>
+
+<div class="centered_justify">
+	<h2>// About //</h2>
+	<p>
+		T.A.S.V. (Timed Automated Service Verifcator) is a bot I developed for a group of friends to
+		help check if their Minecraft server was running, and to send a notification to the admin if the
+		server was down.
+	</p>
+	<p>
+		It didn't see much use and the bot isn't fully configurable (yet), but the code is still
+		available on my GitHub.
+	</p>
+</div>
+
+<div class="centered_justify">
+	<a href="https://www.github.com/Velkee/tasv">
+		<h2>Available Here</h2>
+	</a>
+</div>
