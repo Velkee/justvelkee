8ea937fdf5dde766e0f4f71d2ca89f9c7a8ad614
Velkee <89312879+Velkee@users.noreply.github.com>
2022-12-14 11:33:10 +0100

    Added praxisDB info

---
+++ src/routes/praxisdb/+page.svelte
@@ -0,0 +1,27 @@
+<header>
+	<h1>praxisDB</h1>
+
+	<nav>
+		<a href="/">
+			<p>Back to Home</p>
+		</a>
+	</nav>
+</header>
+
+<div class="centered_justify">
+	<h2>// About //</h2>
+	<p>
+		PraxisDB is a project I made to keep track of YFF praxis positions in companies. The project is
+		currently not feature complete, and the website and API do not communicate yet.
+	</p>
+	<p>
+		If I manage to finish the project, it may serve as a helpfull tool to those who are looking for
+		a praxis position and teachers. A link to the projects's GitHub page is linked below.
+	</p>
+</div>
+
+<div class="centered_justify">
+	<a href="https://www.github.com/Velkee/praxisDB">
+		<h2>Available Here</h2>
+	</a>
+</div>
